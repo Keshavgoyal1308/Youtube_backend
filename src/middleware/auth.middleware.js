3cdcc395fe7f1c67f7dc5d925d10353a883d1247
keshav <goyalkeshav650@gmail.com>
2025-07-30 03:42:45 +0530

    debug some issue in login and logout and made refresh token controller

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,12 +1,14 @@
-import { asyncHandler } from "../utils/asyncHandler";
-import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import ApiError  from "../utils/ApiError.js";
 import jwt from "jsonwebtoken";
 import { User } from "../models/user.model.js";
 
 
 
+
+
 export const verifyJWT= asyncHandler(async (req, res, next) => {
-    req.cookies?.accessToken || req.headers("Authorization")?.replace("Bearer", "");
+    const token = req.cookies?.accessToken || req.headers.Authorization?.replace("Bearer", "");
 
     if(!token){
         throw new ApiError(401, "You are not authorized to access this resource");
@@ -18,6 +20,9 @@ export const verifyJWT= asyncHandler(async (req, res, next) => {
     if(!user){
         throw new ApiError(401, "Invalid Access Token");
     }
+
+    req.user = user; // Attach the user to the request object
+    next(); // Call the next middleware or route handler
 
 
 });
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,6 +3,8 @@ import { registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middleware/multer.middleware.js";
 import { loginUser } from "../controllers/user.controller.js";
 import { verifyJWT } from "../middleware/auth.middleware.js";
+import { logoutUser, refreshAccessToken } from "../controllers/user.controller.js";
+
 
 const router = Router();
 
@@ -20,6 +22,7 @@ router.route("/register").post( upload.fields([
     router.route("/login").post(loginUser);
     //secure routes with JWT verification middleware
     router.route("/logout").post(verifyJWT, logoutUser);
+    router.route("/refresh-token").post(refreshAccessToken)
 
 
 
